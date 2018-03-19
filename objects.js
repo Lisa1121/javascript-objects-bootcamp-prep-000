33834d30ecde5c8abffd42d298a218ff1f9338f5
Lisa Shaughnessy <lisavshaughnessy@gmail.com>
2018-03-19 05:25:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = new Object({artist:'song title'});
 function updatePlaylist(playlist, artist, song) {
   return Object.assign({}, playlist, {[artist]: song})
 }
+function removeFromPlaylist(playlist, artistName) {
+   delete playlist[artistName]
+   return playlist
+}   
